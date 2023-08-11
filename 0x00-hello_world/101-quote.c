8260c8be5cf12a44cbf3866fc88628e62669a1ca
meshackvsyouall <meshackmogire2022@gmail.com>
2023-08-11 04:35:29 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Failure)
+ */
+int main(void)
+{
+const char *message =
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+ssize_t message_length = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n") - 1;
+write(STDERR_FILENO, message, message_length);
+return 1;
+}
