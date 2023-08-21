7fcb0824c98ccbf8dbcab274988e211c3f874f04
meshackvsyouall <meshackmogire2022@gmail.com>
2023-08-20 22:11:50 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - checks for checks for a digit (0 through 9)
+ * @a: a - Variable
+ * @b: b - Variable
+ * Return: always 0
+ */
+
+int mul(inta, int b)
+{
+int mul;
+
+mul = a * b;
+return (mul);
+}
