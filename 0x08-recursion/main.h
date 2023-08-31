4b47d67098e78da0148232e139cd6ed6ca4e5fdc
meshackvsyouall <meshackmogire2022@gmail.com>
2023-08-30 18:47:26 -0700

    6-is_prime_number.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,5 +4,9 @@
 void _puts_recursion(char *s);
 int _putchar(char c);
 void _print_rev_recursion(char *s);
-
+int _strlen_recursion(char *s);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int check_prime(int n, int divisor);
 #endif
